2d73e671bca84a8ecd7de1ebd08cbd7a7f9cdc7d
Vivek Choudhry <126272453+Vivek084c@users.noreply.github.com>
2024-07-13 17:43:00 +0530

    build the model and image procesing done

--- car_tracker/main.py
+++ car_tracker/main.py
@@ -1,29 +1,68 @@
 import cv2
+from utils import get_parking_spot_boxes, is_empty
 
 #defining the input video path
-video_path = "dataset/data/parking_crop_loop.mp4"
+video_path = "dataset/data/parking_1920_1080.mp4"
 
 #definig the mask path
-mask_path = "dataset/mask_crop.png"
+mask_path = "dataset/mask_1920_1080.png"
 
 mask = cv2.imread(mask_path, 0)
 
 #creating opencv video capture
 cap = cv2.VideoCapture(video_path)
 
-connexted_componenets = cv2.connectedComponents(mask, 4, cv2.CV_32S)
+connexted_componenets = cv2.connectedComponentsWithStats(mask, 4, cv2.CV_32S)
+spots = get_parking_spot_boxes(connexted_componenets)
+
+
+#performing classification in every second not 30 times in every second
+step = 30
+spot_state = [None for j in spots]    
+
 
 isFrameAvailabe = True
+frame_num = 0
 while isFrameAvailabe:
     #reading the frame
     isFrameAvailabe, frame = cap.read()
+
+    if frame_num % step == 0:
+
+
+        #itterating over the spots
+        for spot_index, spot in enumerate(spots):
+            x1, y1, w, h = spot
+            
+            #extracting a paticular box from frame
+            spot_crop = frame[y1: y1+h, x1: x1+w, :]
+
+            #checking if it is empty or not
+            spot_status = is_empty(spot_crop)
+            spot_state[spot_index] = spot_status
+
+
+    for spot_index, spot in enumerate(spots):
+        spot_status = spot_state[spot_index]
+        x1, y1, w, h = spots[spot_index]
+        if spot_status:
+            #marking spot green if it is empty
+            cv2.rectangle(frame, (x1, y1), (x1 + w, y1 + h), (0, 255, 0), 2)
+        else:
+            #marking spot red if it is not empty
+            cv2.rectangle(frame, (x1, y1), (x1 + w, y1 + h), (0, 0, 255), 2)
+            
     
+    cv2.namedWindow("frame", cv2.WINDOW_NORMAL)
     #visualising the frame
     cv2.imshow("frame",  frame)
+
 
     #closing the window if q is pressed
     if cv2.waitKey(25)  & 0xFF == ord('q'):
         break
+
+    frame_num += 1
 
 
 cap.release()
---
+++ car_tracker/utils.py
@@ -0,0 +1,61 @@
+import cv2
+from skimage.transform import resize
+import numpy as np 
+import pickle
+
+EMPTY = True
+NOT_EMPTY = False
+
+#loading the model
+model = pickle.load(open("dataset/model/model.p", "rb"))
+
+def is_empty(spot_bgr):
+    """
+    returns weather a given box is having a car parked or not (EMPTY/NOT_EMPTY)
+    Args:
+        spot_bgr: input rgb image
+    Returs:
+        bool: EMPTY/NOT_EMPTY
+    """
+
+    flat_data = []
+
+    #resizing the input image
+    img_resized = resize(spot_bgr, (15, 15, 3))
+    flat_data.append(img_resized.flatten())
+    flat_data = np.array(flat_data)
+
+    #preforming model prediction
+    y_out = model.predict(flat_data)
+
+    if y_out == 0:
+        return EMPTY
+    else:
+        return NOT_EMPTY
+
+
+
+
+def get_parking_spot_boxes(connceted_components):
+    """
+    Returns the list of bouding box in the connected componenets
+    Args:
+        connceted_components: connceted_components object generated using opencv and mask image
+    Returs:
+        slots: list of position vector for each bouding box
+    """
+    (totalLabesl, label_ids, values, centroid) = connceted_components
+
+    slots= [ ]
+    coef = 1
+
+    for i in range(1, totalLabesl):
+        #extracting the corrdinates for each bounding box
+        x1 = int(values[i, cv2.CC_STAT_LEFT] * coef)
+        y1 = int(values[i, cv2.CC_STAT_TOP] * coef)
+        w = int(values[i, cv2.CC_STAT_WIDTH] * coef)
+        h = int(values[i, cv2.CC_STAT_HEIGHT] * coef)
+
+        slots.append([x1, y1, w, h])
+
+    return slots
